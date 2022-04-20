9f2b62cc1a563fc84a0036be63781ebbae0274a1
AndreaScaffidiAbbate <andrea.scaffidi92@gmail.com>
2022-04-20 12:47:56 +0200

    Finished

---
+++ src/it/unikey/Login.java
@@ -0,0 +1,16 @@
+package it.unikey;
+
+import javax.swing.*;
+
+public class Login {
+    public static void main(String[] a) {
+        LoginFrame frame = new LoginFrame();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 370, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+    }
+
+}
---
+++ src/it/unikey/Thread1.java
@@ -0,0 +1,14 @@
+package it.unikey;
+
+import javax.swing.*;
+
+public class Thread1 extends Thread{
+    public void run(){
+        LoginFrame frame = new LoginFrame();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 370, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+    }
+}
